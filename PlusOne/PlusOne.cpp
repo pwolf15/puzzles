dfdd720d41e870a12e23fa33d913a8b333f4aea8
pwolf15 <pm_wolf@comcast.net>
2020-05-24 15:29:35 -0700

    Added efficient solution for plus one

--- PlusOne/PlusOne.cpp
+++ PlusOne/PlusOne.cpp
@@ -7,10 +7,64 @@
 #include <algorithm>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        
+    vector<int> plusOne(vector<int> &digits)
+    {
+        return mostEfficient(digits);
+    }
+
+private:
+
+    vector<int> mostEfficient(vector<int> &digits)
+    {
+        for (int i=digits.size(); i--; digits[i] = 0)
+        {
+            std::cout << digits[i] << std::endl;
+            if (digits[i]++ < 9)
+                return digits;
+        }
+
+        digits[0]++;
+        digits.push_back(0);
+        return digits;
+    }
+
+    vector<int> memEfficientPlusOne(vector<int> &digits)
+    {
+        int carry = 1;
+        int i = digits.size() - 1;
+
+        // iterate over each digit, add carry at a time
+        while (i >= 0)
+        {
+            if (digits[i] == 9 && carry)
+            {
+                digits[i] = 0;
+                carry = 1;
+            }
+            else
+            {
+                digits[i] += carry;
+                carry = 0;
+            }
+
+            --i;
+        }
+
+        if (carry)
+        {
+            // carry only ever 1
+            digits[0] = 1;
+            digits.push_back(0);
+        }
+
+        return digits;
+    }
+
+    vector<int> basicPlusOne(vector<int> &digits)
+    {
         std::vector<int> results;
         int carry = 1;
         int i = digits.size() - 1;
@@ -39,7 +93,7 @@ public:
 bool equals(const vector<int> a, const vector<int> b)
 {
     bool equal = true;
-    
+
     if (a.size() != b.size())
     {
         return false;
@@ -60,17 +114,17 @@ int main()
     Solution sol;
 
     const vector<vector<int>> tests = {
-        { 4, 3, 2, 1 },
-        { 9, 9, 9 }
-    };
+        {4, 3, 2, 1},
+        {9, 9, 9},
+        {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}};
 
     const vector<vector<int>> expected = {
-        { 4, 3, 2, 2 },
-        { 1, 0, 0, 0 }
-    };
+        {4, 3, 2, 2},
+        {1, 0, 0, 0},
+        {9, 8, 7, 6, 5, 4, 3, 2, 1, 1}};
 
     int i = 0;
-    for (auto test: tests)
+    for (auto test : tests)
     {
         auto soln = sol.plusOne(test);
         assert(equals(expected[i], soln));
