b7f6bd91f93388fa7a70a6eae6163ebace9b3807
pwolf15 <pm_wolf@comcast.net>
2020-07-09 00:34:02 -0700

    Added '3Sum'

--- Arrays/3Sum/3Sum.cpp
+++ Arrays/3Sum/3Sum.cpp
@@ -2,21 +2,26 @@
 #include <vector>
 #include <math.h>
 #include <algorithm>
+#include <unordered_map>
 
 class Solution
 {
 public:
     std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
     {
+        return threeSum_sorted(nums);
+    }
 
+    std::vector<std::vector<int>> threeSum_bruteForce(std::vector<int> &nums)
+    {
         std::vector<std::vector<int>> sols;
         std::sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); i++)
         {
-            for (int j = 1; j < nums.size(); j++)
+            for (int j = i + 1; j < nums.size(); j++)
             {
                 int result = nums[i] + nums[j];
-                for (int k = 2; k < nums.size(); k++)
+                for (int k = j + 1; k < nums.size(); k++)
                 {
                     if (((i != j) && (j != k) && (i != k)) && (result + nums[k]) == 0)
                     {
@@ -43,6 +48,81 @@ public:
 
         return sols;
     }
+
+    std::vector<std::vector<int>> threeSum_sorted(std::vector<int>& nums)
+    {
+        std::vector<std::vector<int>> triples;
+
+        if (nums.size() < 3)
+        {
+            return triples;
+        }
+
+        std::sort(nums.begin(), nums.end());
+        int size = nums.size();
+        int prev = nums[0] - 1;
+        for (int i = 0; i < size; i++)
+        {
+            // std::cout << i << std::endl;
+
+            if (nums[i] == prev)
+            {
+                continue;
+            }
+
+            int target      = -nums[i];
+
+            int first       = i + 1;
+            if (first >= size)
+            {
+                break;
+            }
+            int prevFirst   = nums[first] - 1;
+            int last        = nums.size() - 1;
+            int prevLast    = nums[last] + 1;
+
+            while (first < last)
+            {
+                // std::cout << "first, " << first << ", last, " << last << std::endl;
+                if (first >= size || last < 0)
+                {
+                    break;
+                }
+
+                if (prevFirst == nums[first])
+                {
+                    first++;
+                    continue;
+                }
+                if (prevLast == nums[last])
+                {
+                    last--;
+                    continue;
+                }
+
+                int sum = nums[first] + nums[last];
+                if (sum < target)
+                {
+                    first++;
+                }
+                else if (sum > target)
+                {
+                    last--;
+                }
+                else
+                {
+                    triples.push_back({ nums[i], nums[first], nums[last], });
+
+                    prevFirst = nums[first];
+                    prevLast = nums[last];
+                }
+            }
+
+            prev = nums[i];
+        }
+
+        return triples;
+    }
 };
 
 int main()
@@ -50,6 +130,29 @@ int main()
     Solution sol;
     std::vector<int> inCells = {-1, 0, 1, 2, -1, -4};
     auto result = sol.threeSum(inCells);
+    // for (auto triplet : result)
+    // {
+    //     std::cout << "Solution!" << std::endl;
+
+    //     for (auto member : triplet)
+    //     {
+    //         std::cout << member << ", ";
+    //     }
+
+    //     std::cout << "\n";
+    // }
+
+    inCells = {};
+
+    sol.threeSum(inCells);
+
+    inCells = {0};
+
+    sol.threeSum(inCells);
+
+    inCells = {1,2,-2,-1};
+
+    result = sol.threeSum(inCells);
     for (auto triplet : result)
     {
         std::cout << "Solution!" << std::endl;
@@ -61,6 +164,7 @@ int main()
 
         std::cout << "\n";
     }
+
 
     return 0;
 }
