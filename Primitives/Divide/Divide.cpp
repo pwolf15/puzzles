ac7ad77356a4c6d11627ace414f887571c8f60ef
pwolf15 <pm_wolf@comcast.net>
2020-07-13 21:33:16 -0700

    Added brute-force divide

--- Primitives/Divide/Divide.cpp
+++ Primitives/Divide/Divide.cpp
@@ -46,14 +46,56 @@ unsigned long long add_bw(unsigned long long x, unsigned long long y)
     return x;
 }
 
+// O(x / y) brute force => O(x)
+// if y = 1, x is very large it's going to take a while
+int Divide_BF(int x, int y)
+{
+    int result = 0;
+    while (x > 0)
+    {
+        x -= y;
+        result++;
+    }
+
+    if (x < 0)
+    {
+        result--;
+    }
+
+    return result;
+}
+
+// relate x / y to (x - y) / y
+// 3 / 1 => (3 - 1) / 1
+// 100011 (35)
+// 100011 - 111 => 11100 (28) (35 - 32) => 2^5 
+// 35 - 32 = 2^5
+// 111 (7)
+// 4 / 2 => (4 - 2) / 2
+// 35 / 7 => (35 - 7) / 7, 28 / 7 = 4, 
+// 35 x / y => (x - y) / y 35 => 28
+// 35 / 7 => 5, 4
+// 63 / 21 => 3, 42 / 21 => 2
+// 11 / 4 => 2 (1011) => 11 - 8 => (2^3) + 2^1 + 2^0
+// 35 / 7
+// 4 / 2 = (110 / 10) => 110 => 1 11 - 10 => 1
+// 100 10  >> 1 = 10
+// 110 10 >> 1 11 > 
+// 35 / 7 
+// 100011
+// 111
+// 10010
+// 111
+// 10010 18
+// 00111  7
+//  1011  11
+//  0111  7
+//   100  4
+// 10010 18
+// 00111  7
+
 int main()
 {
-    // std::cout << Multiply_bit(7, 5) << std::endl;
-    std::cout << 0b100011 << std::endl;
-    std::cout << add_bw(1, 1) << std::endl;
-    std::cout << add_bw(10000, 10000) << std::endl;
-    std::cout << Multiply_bw(3, 3) << std::endl;
-    std::cout << Multiply_bw(10000, 10000) << std::endl;
-
+    std::cout << Divide_BF(100, 3) << std::endl;
     return 0;
 }
