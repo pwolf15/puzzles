e7ee93b34ee061814f8abf9d67178ce185dd4287
pwolf15 <pm_wolf@comcast.net>
2020-07-16 00:15:57 -0700

    Added Primitive 'Divide'

--- Primitives/Divide/Divide.cpp
+++ Primitives/Divide/Divide.cpp
@@ -65,37 +65,44 @@ int Divide_BF(int x, int y)
     return result;
 }
 
-// relate x / y to (x - y) / y
-// 3 / 1 => (3 - 1) / 1
-// 100011 (35)
-// 100011 - 111 => 11100 (28) (35 - 32) => 2^5 
-// 35 - 32 = 2^5
-// 111 (7)
-// 4 / 2 => (4 - 2) / 2
-// 35 / 7 => (35 - 7) / 7, 28 / 7 = 4, 
-// 35 x / y => (x - y) / y 35 => 28
-// 35 / 7 => 5, 4
-// 63 / 21 => 3, 42 / 21 => 2
-// 11 / 4 => 2 (1011) => 11 - 8 => (2^3) + 2^1 + 2^0
-// 35 / 7
-// 4 / 2 = (110 / 10) => 110 => 1 11 - 10 => 1
-// 100 10  >> 1 = 10
-// 110 10 >> 1 11 > 
-// 35 / 7 
-// 100011
-// 111
-// 10010
-// 111
-// 10010 18
-// 00111  7
-//  1011  11
-//  0111  7
-//   100  4
-// 10010 18
-// 00111  7
+int Divide(int x, int y)
+{
+    // how many times does k * 2^n (think like long division)
+    int quotient = 0;
+    while (x >= y)
+    {
+        int logX = log2(x);
+        int logY = log2(y);
+        int operand = y << (logX - logY);
+        int shiftAmt = (logX - logY);
+        if (operand > x)
+        {
+            operand >>= 1;
+            shiftAmt -= 1;
+        }
+
+        #ifdef DEBUG
+        std::cout << "x: " << x << std::endl;
+        std::cout << "log2x: " << logX << std::endl;
+        std::cout << "log2y: " << logY << std::endl;
+        std::cout << "quotient: " << quotient << std::endl;
+        std::cout << "dividend: " << operand << std::endl;
+        #endif
+
+        x -= operand;
+        quotient += 1 << shiftAmt;
+    }
+
+    return quotient;
+}
 
 int main()
 {
-    std::cout << Divide_BF(100, 3) << std::endl;
+    std::cout << Divide(7, 4) << std::endl;
+    std::cout << Divide(225, 15) << std::endl;
+    std::cout << Divide(100, 3) << std::endl;
+    std::cout << Divide(1e6, 1) << std::endl;
+    std::cout << Divide(189234, 23) << std::endl;
+    
     return 0;
 }
