4fa5747c659d9fd40caa00f0d83e260c7184534f
pwolf15 <pm_wolf@comcast.net>
2020-07-17 19:31:05 -0700

    Added 'Reverse' brute-force

--- Primitives/Reverse/Reverse.cpp
+++ Primitives/Reverse/Reverse.cpp
@@ -3,120 +3,32 @@
 #include <cmath>
 #include <limits>
 
-unsigned long long ReverseBits_BruteForce(unsigned long long x)
+long long Reverse_BF(int x)
 {
-    size_t numBits = sizeof(unsigned long long) * 8;
-
-    for (int i = 0; i < numBits / 2; ++i)
+    std::string xStr = std::to_string(x);
+    std::string reversed = "";
+    
+    int idx = 0;
+    if (xStr[0] == '-')
     {
-        long long hBit = (x >> (numBits - i - 1)) & 0x1;
-        long long lBit = (x >> i) & 0x1;
-
-        // unset existing bits
-        x &= ~(1LL << (numBits - i - 1));
-        x &= ~(1LL << i);
-        x |= (lBit << (numBits - i - 1));
-        x |= (hBit << i);
+        reversed +=  '-';
+        idx++;
     }
 
-    return x;
+    for (int i = xStr.size() - 1; i >= idx; --i)
+    {
+        reversed += xStr[i];
+    }
+
+    return std::stoull(reversed);
 }
-
-std::vector<unsigned long long> reversed;
-bool initCache = false;
-
-unsigned long long ReverseBits_Cached(unsigned long long x)
-{
-    if (!initCache)
-    {
-        size_t shortMax = std::numeric_limits<unsigned short>::max();
-        reversed.resize(shortMax + 1);
-        for (size_t num = 0; num < reversed.size(); num++)
-        {
-            int numBits = sizeof(unsigned short) * 8;
-            unsigned short result = num;
-            for (int i = 0; i < numBits / 2; ++i)
-            {
-                long long hBit = (result >> (numBits - i - 1)) & 0x1;
-                long long lBit = (result >> i) & 0x1;
-
-                // unset existing bits
-                result &= ~((short)1L << (numBits - i - 1));
-                result &= ~((short)1L << i);
-                result |= (lBit << (numBits - i - 1));
-                result |= (hBit << i);
-            }
-
-            reversed[num] = result;
-        }
-
-        initCache = true;
-    }
-
-    size_t mask = 0xffff;
-    size_t maskSize = 16;
-
-    // complexity is O(n / L) where n is n-bit integer and L is number of bits in cached keys
-    return ((long long)reversed[(x >> maskSize * 3) & mask]) |
-           (((long long)reversed[(x >> maskSize * 2) & mask]) << maskSize) |
-           (((long long)reversed[(x >> maskSize) & mask]) << maskSize * 2) |
-           (((long long)reversed[x & mask]) << maskSize * 3);
-}
-
-class Solution
-{
-public:
-    uint32_t reverseBits(uint32_t n)
-    {
-        if (!initCache)
-        {
-            size_t shortMax = std::numeric_limits<unsigned char>::max();
-            reversed.resize(shortMax + 1);
-            for (size_t num = 0; num < reversed.size(); num++)
-            {
-                int numBits = sizeof(unsigned char) * 8;
-                unsigned short result = num;
-                for (int i = 0; i < numBits / 2; ++i)
-                {
-                    long long hBit = (result >> (numBits - i - 1)) & 0x1;
-                    long long lBit = (result >> i) & 0x1;
-
-                    // unset existing bits
-                    result &= ~((short)1L << (numBits - i - 1));
-                    result &= ~((short)1L << i);
-                    result |= (lBit << (numBits - i - 1));
-                    result |= (hBit << i);
-                }
-
-                reversed[num] = result;
-            }
-
-            initCache = true;
-        }
-
-        size_t mask = 0xff;
-        size_t maskSize = 8;
-
-        // complexity is O(n / L) where n is n-bit integer and L is number of bits in cached keys
-        return ((long long)reversed[(n >> maskSize * 3) & mask]) |
-               (((long long)reversed[(n >> maskSize * 2) & mask]) << maskSize) |
-               (((long long)reversed[(n >> maskSize) & mask]) << maskSize * 2) |
-               (((long long)reversed[n & mask]) << maskSize * 3);
-    }
-private:
-    std::vector<uint32_t> reversed;
-    bool initCache = false;
-};
 
 int main()
 {
-    long long result = ReverseBits_BruteForce(0xa);
-    // std::cout << "result: 0x" << std::hex << result << std::dec << std::endl;
-    // std::cout << "result: 0x" << std::hex << ReverseBits_BruteForce(0xdeadbeef) << std::dec << std::endl;
-    std::cout << "result: 0x" << std::hex << ReverseBits_Cached(0xffffffff) << std::endl;
-
-    Solution sol;
-    std::cout << std::hex << sol.reverseBits(0x1) << std::dec << std::endl;
+    long long result = Reverse_BF(314);
+    std::cout << result << std::endl;
+    result = Reverse_BF(-314);
+    std::cout << result << std::endl;
 
     return 0;
 }
