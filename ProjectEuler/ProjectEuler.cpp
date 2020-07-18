71c49c76f1abe5f609e7ca44c1611ec7ac84d4bc
pwolf15 <pm_wolf@comcast.net>
2020-07-17 20:49:45 -0700

    Added 'LargestPrimeFactor'

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -48,13 +48,39 @@ unsigned long long sumOfEvenFib()
     return sum;
 }
 
+unsigned long long primeFactor(unsigned long long n)
+{
+    int prod = 1;
+    int i = 2;
+    for (; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            std::cout << "i: " << i << std::endl;
+            n = n / (prod * i);
+            // std::cout << "n: " << n << std::endl;
+            i = 2;
+        }
+    }
+
+    return i;
+}
 int main()
 {
-
+    #define PRIME_FACTOR
+    #ifdef SUM_OF_MULTIPLES_35
     std::cout << sumOfMultiples3And5(10) << std::endl;
     std::cout << sumOfMultiples3And5(1000) << std::endl;
+    #endif
 
+    #ifdef EVEN_FIB
     std::cout << sumOfEvenFib() << std::endl;
+    #endif
+
+    #ifdef PRIME_FACTOR
+    std::cout << primeFactor(13195) << std::endl;
+    std::cout << primeFactor(600851475143) << std::endl;
+    #endif
 
     return 0;
 }
