3e8867d25191fca30db1ff4958a0b7ac7ff2a979
pwolf15 <pm_wolf@comcast.net>
2020-07-19 19:41:33 -0700

    Added 'IsPalindromeNumber'

--- Primitives/IsPalindromeNumber/IsPalindromeNumber.cpp
+++ Primitives/IsPalindromeNumber/IsPalindromeNumber.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <math.h>
 
+// O(n) time complexity, O(n) space complexity
 bool IsPalindromeNumber_BF(int x)
 {
     std::string xStr = std::to_string(x);
@@ -21,13 +22,84 @@ bool IsPalindromeNumber_BF(int x)
     return true;
 }
 
+// O(n): n digits in x
+long long Reverse(int x)
+{
+    long long result = 0;
+
+    while (x)
+    {
+        result = result * 10 + x % 10;
+        x /= 10;
+    }
+
+    return result;
+}
+
+// O(n) time complexity, O(n) space complexity
+bool IsPalindromeNumber_Reverse(int x)
+{
+    return x == Reverse(x);
+}
+
+// O(n) time complexity, O(n) space complexity
+bool IsPalindromeNumber_SpaceEfficient(int x)
+{
+    if (x <= 0)
+    {
+        return x == 0;
+    }
+
+    int msd = log10(x);
+    int msd_mask = pow(10, msd);
+    bool match = true;
+    while (x)
+    {
+        int largestDigit = (x / msd_mask);
+        int smallestDigit = x % 10;
+
+        #ifdef DEBUG
+        std::cout << "MSD: " << msd << std::endl;
+        std::cout << "MSD mask: " << msd_mask << std::endl;
+        std::cout << "LD: " << largestDigit << "\n";
+        std::cout << "SD: " << smallestDigit << "\n";
+        #endif
+
+        if (largestDigit != smallestDigit)
+        {
+            match = false;
+            break;
+        }
+
+        x %= msd_mask;      // remove MSD
+        x /= 10;            // remove LSD
+        msd_mask /= 100;
+    }
+
+    return match;
+}
+
 int main()
 {
-    std::cout << IsPalindromeNumber_BF(314) << std::endl;
-    std::cout << IsPalindromeNumber_BF(12344321) << std::endl;
-    std::cout << IsPalindromeNumber_BF(0) << std::endl;
-    std::cout << IsPalindromeNumber_BF(12321) << std::endl;
+    std::vector<int> tests = {314, 12344321, 0, 12321, 12343321};
 
+#ifdef RUNALL
+    for (auto test : tests)
+    {
+        std::cout << IsPalindromeNumber_BF(test) << std::endl;
+    }
+
+    for (auto test : tests)
+    {
+        std::cout << IsPalindromeNumber_Reverse(test) << std::endl;
+    }
+#endif
+
+    for (auto test : tests)
+    {
+        bool palindrome = IsPalindromeNumber_SpaceEfficient(test);
+        std::cout << "Palindrome: " << palindrome << std::endl;
+    }
 
     return 0;
 }
