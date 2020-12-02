789e2d649700d54c9748edb4da8401f445f1db56
pwolf15 <patrick.wolf@live.com>
2020-12-01 22:47:43 -0800

    Added solutions for 'Day 2'

--- AdventOfCode/main.cpp
+++ AdventOfCode/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 int computeProduct2(std::string filename)
 {
@@ -82,9 +83,117 @@ int computeProduct3(std::string filename)
     return product;
 }
 
+struct PasswordMetadata
+{
+    std::string password;
+    char target;
+    std::vector<int> range;
+};
+
+PasswordMetadata getMetadata(std::string passwordString)
+{
+    PasswordMetadata metadata;
+    metadata.range.resize(2);
+
+    std::string minStr;
+    {
+        minStr = passwordString.substr(0, passwordString.find("-"));
+    }
+    
+    std::string maxStr;
+    {
+        maxStr = passwordString.substr(passwordString.find("-") + 1, passwordString.find(" ") - passwordString.find("-"));
+    }
+
+    char c;
+    {
+        c = passwordString.substr(passwordString.find(":") - 1, 1)[0];
+    }
+
+    std::string password;
+    {
+        password = passwordString.substr(passwordString.find(":") + 1);
+    }
+
+    metadata.range[0] = std::atoi(minStr.c_str());
+    metadata.range[1] = std::atoi(maxStr.c_str());
+    metadata.target = c;
+    metadata.password = password;
+
+    std::cout << "Min: " << minStr << std::endl;
+    std::cout << "Max: " << maxStr << std::endl;
+    std::cout << "Target: " << c << std::endl;
+    std::cout << "Password: " << password << std::endl;
+
+    return metadata;
+}
+
+bool validatePassword(PasswordMetadata metadata)
+{
+    int targetCount = std::count(metadata.password.begin(), metadata.password.end(), metadata.target);
+
+    if (targetCount >= metadata.range[0] && targetCount <= metadata.range[1])
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool validatePassword2(PasswordMetadata metadata)
+{
+    std::vector<int> positions;
+    for (size_t i = 0; i < metadata.password.size(); ++i)
+    {
+        if (metadata.password[i] == metadata.target)
+        {
+            positions.push_back(i);
+        }
+    }
+
+    if ((std::find(positions.begin(), positions.end(), metadata.range[0]) != positions.end()) &&
+        (std::find(positions.begin(), positions.end(), metadata.range[1]) == positions.end()) ||
+        (std::find(positions.begin(), positions.end(), metadata.range[0]) == positions.end()) &&
+        (std::find(positions.begin(), positions.end(), metadata.range[1]) != positions.end()))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int numValidPasswords(std::string filename)
+{
+    int numValid = 0;
+
+    std::ifstream infile(filename);
+    if (!infile.good())
+    {
+        std::cout << "Could not open file." << std::endl;
+        return numValid;
+    }
+
+    std::vector<std::string> passwordStrings;
+    std::string line;
+
+    while (getline(infile, line))
+    {
+        passwordStrings.push_back(line);
+        auto metadata = getMetadata(line);
+        if (validatePassword2(metadata)) numValid++;
+    }
+
+    std::cout << "Num password strings: " << passwordStrings.size() << std::endl;
+    std::cout << "Num valid passwords: " << numValid << std::endl;
+
+    return numValid;
+}
+
 int main()
 {
-    computeProduct2("../day1.txt");
-    computeProduct3("../day1.txt");
+    // computeProduct2("../day1.txt");
+    // computeProduct3("../day1.txt");
+    // numValidPasswords("../day2.txt");
+
     return 0;
 }
