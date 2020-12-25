8aa891eab81583d83b1eca2c01bbcdb2ba23ecfd
pwolf15 <patrick.wolf@live.com>
2020-12-25 10:57:00 -0800

    Added start of reorder function

--- AdventOfCode/day20/main.cpp
+++ AdventOfCode/day20/main.cpp
@@ -48,11 +48,6 @@ struct Tile
     Edges edges;
 };
 
-void flip(Tile& tile)
-{
-
-}
-
 void get_edges(std::vector<std::string> tile_contents, Tile& tile)
 {
     int top = 0;
@@ -138,11 +133,118 @@ void get_tiles(const std::vector<std::string>& lines, std::vector<Tile>& tiles)
     print_tile(tile);
 }
 
+Tile flip(Tile tile)
+{
+    Tile flipped = tile;
+    std::swap(flipped.edges.top, flipped.edges.bottom);
+
+    int new_left = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        new_left |= ((flipped.edges.left >> i) & 0x1) << (10 - i - 1);
+    }
+
+    flipped.edges.left = new_left;
+
+    int new_right = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        new_right |= ((flipped.edges.right >> i) & 0x1) << (10 - i - 1);
+    }
+
+    flipped.edges.right = new_right;
+
+    return flipped;
+}
+
+void reorder(std::vector<Tile> tiles)
+{
+    std::vector<std::vector<int>> image;
+    for (int i = 0; i < 3; i++)
+    {
+        std::vector<int> image_row;
+        for (int j = 0; j < 3; j++)
+        {
+            image_row.push_back(-1);
+        }
+        image.push_back(image_row);
+    }
+    
+    while (true)
+    {
+        for (auto tile: tiles)
+        {
+            std::unordered_map<int, Tile> remaining_tiles;
+            for (auto t: tiles)
+            {
+                remaining_tiles[t.id] = t;
+            }
+            
+            remaining_tiles.erase(tile.id);
+            image[0][0] = tile.id;
+
+            // try right
+            bool foundRight = false;
+            for (auto r: remaining_tiles)
+            {
+                if (r.second.edges.left == tile.edges.right)
+                {
+                    std::cout << "Found possible right: " << tile.id << std::endl;
+                    image[0][1] = r.second.id;
+                    remaining_tiles.erase(r.second.id);
+                    foundRight = true; 
+                    break;
+                }
+                else
+                {
+                    std::cout << tile.edges.right << "!=" << r.second.edges.left << std::endl;
+                }
+            }
+
+            if (!foundRight)
+            {
+                std::cout << "No match for " << tile.id << "!" << std::endl;
+                Tile flipped = flip(tile);
+                print_tile(flipped);
+            }
+
+            tile = flip(tile);
+            image[0][0] = tile.id;
+
+            // try right
+            foundRight = false;
+            for (auto r: remaining_tiles)
+            {
+                if (r.second.edges.left == tile.edges.right)
+                {
+                    std::cout << "Found possible right: " << r.second.id << std::endl;
+                    image[0][1] = r.second.id;
+                    remaining_tiles.erase(r.second.id);
+                    foundRight = true; 
+                    break;
+                }
+                else
+                {
+                    std::cout << tile.edges.right << "!=" << r.second.edges.left << std::endl;
+                }
+            }
+
+            exit(1);
+
+            std::cout << "Size: " << remaining_tiles.size() << std::endl;
+        }
+
+        break;
+    }
+    
+}
+
 int main()
 {
     std::vector<std::string> lines = get_lines("../day20.txt");
     std::vector<Tile> tiles;
     get_tiles(lines, tiles);
+    reorder(tiles);
 
 
     std::cout << "Num lines: " << lines.size() << std::endl;
