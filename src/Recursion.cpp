e410b78870c8121b67412cc8c3c3588382fbe389
pwolf15 <patrick.wolf@live.com>
2021-01-09 14:09:11 -0800

    Added complexity comments to Gcd

--- src/Recursion.cpp
+++ src/Recursion.cpp
@@ -1,5 +1,7 @@
 #include "Recursion.hpp"
 
+// O(log max(x, y)) or O(n) where n is number of bits needed to reprsent inputs
+// space complexity: O(n) (maximum depth of call stack)
 long long Gcd(long long x, long long y) 
 {
     return y == 0 ? x : Gcd(y, x % y);
