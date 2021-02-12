687d3cafe6d49c0474bafb6edd4497e3c7fcf1f1
pwolf15 <patrick.wolf@live.com>
2021-02-12 15:58:16 -0800

    Added 'numberOfSteps' Python

--- puzzles_python/puzzles.py
+++ puzzles_python/puzzles.py
@@ -2,5 +2,39 @@
 def countSegments(s):
     return len(s.split())
 
+def addStrings(num1, num2):
+    """
+    :type num1: str
+    :type num2: str
+    :rtype: str
+    """
+
+    less = num1 if len(num1) < len(num2) else num2
+    greater = num1 if len(num2) >= len(num1) else num2
+
+    return ""
+
+def numberOfSteps (num):
+    """
+    :type num: int
+    :rtype: int
+    """
+
+    steps = 0
+    while num:
+        steps += 1
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num -= 1
+
+    return steps
+
+
 print(countSegments("welcome to the jungle"))
 print(countSegments("Hello, my name is John"))
+print(addStrings("12", "23"))
+print(numberOfSteps(1))
+print(numberOfSteps(14))
+print(numberOfSteps(7))
+print(numberOfSteps(6))
