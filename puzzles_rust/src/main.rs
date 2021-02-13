6d036729abd2fcc9c4c2de214c8ae9dbb6e222e8
pwolf15 <patrick.wolf@live.com>
2021-02-12 16:03:34 -0800

    Added 'num_steps' Rust solution

--- puzzles_rust/src/main.rs
+++ puzzles_rust/src/main.rs
@@ -11,10 +11,32 @@ fn count_segments(s: String) -> i32 {
     return segments;
 }
 
+fn num_steps(num: i32) -> i32 {
+
+    let mut rem = num;
+    let mut num_steps = 0;
+
+    while rem != 0
+    {
+        num_steps += 1;
+        if rem % 2 == 0
+        {
+            rem /= 2;
+        }
+        else 
+        {
+            rem -= 1;
+        }
+    }
+
+    return num_steps;
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{}", count_segments("Hello, world!".to_string()));
     println!("{}", count_segments("welcome to the jungle".to_string()));
     println!("{}", count_segments("".to_string()));
     println!("{}", count_segments("hello".to_string()));
+    println!("{}", num_steps(14));
 }
