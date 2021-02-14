90491f2c0c998dbbbf88f1fd1d0162288425b4e0
pwolf15 <patrick.wolf@live.com>
2021-02-14 15:56:15 -0800

    Started adding 'LinkedList' definition

--- puzzles_rust/src/main.rs
+++ puzzles_rust/src/main.rs
@@ -32,6 +32,33 @@ fn num_steps(num: i32) -> i32 {
     return num_steps;
 }
 
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut head = l1;
+    while head != None
+    {
+        let unwrapped = head.unwrap();
+        println!("{}", unwrapped.val);
+        head = unwrapped.next;
+    }
+    return l2;
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{}", count_segments("Hello, world!".to_string()));
@@ -39,4 +66,10 @@ fn main() {
     println!("{}", count_segments("".to_string()));
     println!("{}", count_segments("hello".to_string()));
     println!("{}", num_steps(14));
+
+    let list: Option<Box<ListNode>> = Some(Box::new(ListNode::new(23)));
+    let list2: Option<Box<ListNode>> = Some(Box::new(ListNode::new(23)));
+    let result = add_two_numbers(list, list2);
+    // println!("{}", list.unwrap().val);
+    println!("{}", result.unwrap().val);
 }
