ca2cc413af628b28d8e4cbf58f013012646a0043
pwolf15 <patrick.wolf@live.com>
2021-03-07 13:49:24 -0800

    Added 'StackWithMax'

--- include/Stacks.hpp
+++ include/Stacks.hpp
@@ -3,6 +3,52 @@
 
 #include "LinkedLists.hpp"
 
+#include <stack>
+#include <map>
+
 void PrintLinkedListInReverse(std::shared_ptr<ListNodeEPI<int>> head);
+
+class StackWithMax
+{
+public: 
+    bool Empty() const {
+        return s.empty();
+    }
+
+    int Max() const {
+        return m.rbegin()->first;
+    }
+
+    int Pop() {
+        if (s.empty())
+        {
+            return -1;
+        }
+        int v = s.top();
+        m.find(v)->second--;
+        if (m.find(v)->second == 0)
+        {
+            m.erase(v);
+        }
+        s.pop();
+        return v;
+    }
+
+    void Push(int x) {
+        s.push(x);
+
+        if (m.find(x) == m.end())
+        {
+            m[x] = 1;
+        }
+        else
+        {
+            m.find(x)->second++;
+        }
+    }
+private:
+    std::stack<int> s;
+    std::map<int, int> m;
+};
 
 #endif // STACKS_HPP
--- test/src/StackTests.cpp
+++ test/src/StackTests.cpp
@@ -1,7 +1,9 @@
-#include "CppUTest/TestHarness.h"
+#include <iostream>
 
-#include "Stacks.hpp"
 #include "LinkedLists.hpp"
+#include "Stacks.hpp"
+
+#include "CppUTest/TestHarness.h"
 
 TEST_GROUP(Stacks)
 {
@@ -12,4 +14,23 @@ TEST(Stacks, PrintLinkedListInReverse)
 {
     std::shared_ptr<ListNodeEPI<int>> l1 = createList({ 3, 1, 4 });
     PrintLinkedListInReverse(l1);
+};
+
+TEST(Stacks, StackWithMax)
+{
+    StackWithMax s;
+    for (int i = 0; i < 10; ++i)
+    {
+        s.Push(i);
+        s.Push(i);
+        s.Push(i);
+    }
+
+    std::vector<int> expected = { 9, 9, 9, 8, 8 };
+    for (int i = 0; i < 5; ++i)
+    {
+        int m = s.Max();
+        CHECK_EQUAL(expected[i], m);
+        s.Pop();
+    }
 };
