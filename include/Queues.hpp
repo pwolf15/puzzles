32a7e8c29d209f254f92b2e4782968ae7f4486f9
pwolf15 <patrick.wolf@live.com>
2021-04-10 22:42:07 -0700

    Added more efficient peek for queue.

--- include/Queues.hpp
+++ include/Queues.hpp
@@ -5,7 +5,7 @@ class MyQueue {
 public:
     /** Initialize your data structure here. */
     MyQueue() {
-        
+        front = -1;
     }
     
     // time complexity: O(1)
@@ -25,6 +25,7 @@ public:
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
 
+        int oldFront = front;
         while (!m_a.empty())
         {
             front = m_a.top();
@@ -40,42 +41,22 @@ public:
             }
         }
 
+        if (!m_b.empty())
+            front = m_b.top();
+
         while (!m_b.empty())
         {
             m_a.push(m_b.top());
             m_b.pop();
         }
 
-        return front;
+        return oldFront;
     }
     
-    // time complexity: O(n)
+    // time complexity: O(1)
     // space complexity: O(n)
     /** Get the front element. */
     int peek() {
-        
-        while (!m_a.empty())
-        {
-            front = m_a.top();
-            m_a.pop();
-
-            if (m_a.empty())
-            {
-                m_b.push(front);
-                break;
-            }
-            else
-            {
-                m_b.push(front);
-            }
-        }
-
-        while (!m_b.empty())
-        {
-            m_a.push(m_b.top());
-            m_b.pop();
-        }
-
         return front;
     }
     
