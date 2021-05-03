20f02a918fe25a4ce1439a9b0135f86988debaf5
pwolf15 <patrick.wolf@live.com>
2021-05-02 20:17:29 -0700

    Added more efficient 'CountStudents'

--- src/Queues.cpp
+++ src/Queues.cpp
@@ -2,7 +2,7 @@
 
 // time complexity: O(n)
 // space complexity: O(n)
-int countStudents(std::vector<int>& students, std::vector<int>& sandwiches)
+int countStudents_PW1(std::vector<int>& students, std::vector<int>& sandwiches)
 {
     std::queue<int> studQueue;
     std::queue<int> sandQueue;
@@ -35,4 +35,27 @@ int countStudents(std::vector<int>& students, std::vector<int>& sandwiches)
     }
 
     return studQueue.size();
+}
+
+// time complexity: O(n)
+// space complexity: O(1)
+int countStudents_PW2(std::vector<int>& students, std::vector<int>& sandwiches)
+{
+    std::vector<int> count = {0, 0};
+    int n = students.size();
+    int k;
+    for (int s: students)
+    {
+        count[s]++;
+    }
+    for (k = 0; k < n && count[sandwiches[k]] > 0; ++k)
+    {
+        count[sandwiches[k]]--;
+    }
+    return n - k;
+}
+
+int countStudents(std::vector<int>& students, std::vector<int>& sandwiches)
+{
+    return countStudents_PW2(students, sandwiches);
 }
