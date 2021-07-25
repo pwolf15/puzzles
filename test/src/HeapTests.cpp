1cee7fe03920b2cad4cce78c8f91b2d2a4413eb0
pwolf15 <patrick.wolf@live.com>
2021-07-25 13:44:49 -0700

    Added 'TopK' test

--- test/src/HeapTests.cpp
+++ test/src/HeapTests.cpp
@@ -12,5 +12,12 @@ TEST_GROUP(Heaps)
 
 TEST(Heaps, TopK)
 {
-  
+  std::vector<std::string> expected = { "hello", "google","patrick"};
+  std::vector<std::string> input = { "hello", "my", "name", "is", "patrick", "this", "can", "be", "google"};
+  std::vector<std::string> result = TopK(3, input.cbegin(), input.cend());
+  CHECK_EQUAL(expected.size(), result.size());
+  for (size_t i = 0; i < expected.size(); ++i)
+  {
+    CHECK_EQUAL(expected[i], result[i]);
+  }
 };
