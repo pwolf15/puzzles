00afb2e3f7f6fcdbeb05a989a35ad87ecdfe5d41
pwolf15 <patrick.wolf@live.com>
2021-09-27 21:48:54 -0700

    Added 'SearchFirstOfK'

--- src/Search.cpp
+++ src/Search.cpp
@@ -106,7 +106,33 @@ int SearchFirstOfK_PW2(std::vector<int>& A, int k)
   return cur;
 }
 
+// time complexity: O(log n)
+// space complexity: O(1)
+int SearchFirstOfK_EPI(std::vector<int>& A, int k)
+{
+  int left = 0, right = std::size(A) - 1, result = -1;
+  // A[left, right] is the candiadate set.
+  while (left <= right)
+  {
+    if (int mid = left + ((right - left) / 2); A[mid] > k) 
+    {
+      right = mid - 1;
+    }
+    else if (A[mid] == k) 
+    {
+      result = mid;
+      // Nothing to the right of mid can be the first occurrence of k
+      right = mid - 1;
+    }
+    else // A[mid] < k.
+    {
+      left = mid + 1;
+    }
+  }
+  return result;
+}
+
 int SearchFirstOfK(std::vector<int>& A, int k)
 {
-  return SearchFirstOfK_PW2(A, k);
+  return SearchFirstOfK_EPI(A, k);
 }
